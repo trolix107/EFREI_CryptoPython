1e4f9fc375a0f9cecb366d41fd665dfdd310588f
trolix107 <gregoire.barix@edu.esiee-it.fr>
2025-01-15 12:50:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+
 from cryptography.fernet import Fernet
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
@@ -9,7 +10,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('maison.svg')
+    return render_template('manette.svg')
 
 @app.route('/jack')
 def exo_jack():
