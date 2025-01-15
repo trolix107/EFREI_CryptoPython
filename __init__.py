7edc81c6d49acff19086306be79d49075ce6ffbf
trolix107 <gregoire.barix@edu.esiee-it.fr>
2025-01-15 11:55:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('chenille.html')
+    return render_template('chenille.svg')
 
 @app.route('/jack')
 def exo_jack():
