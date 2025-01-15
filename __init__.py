a6a14d02139423bef51f5fcaa5dfc11dd00e2510
trolix107 <gregoire.barix@edu.esiee-it.fr>
2025-01-15 11:15:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('exercice1.html') # hh
+    return render_template('maison.svg') # hh
   
 key = Fernet.generate_key()
 f = Fernet(key)
